1d4fb21f61a6ba56c938cd052ed50e2f9afa3ac9
Abhishek-Balyani <abbut1780@gmail.com>
2024-05-20 10:29:22 +0530

    bottom navigation bar ui

--- lib/pages/bottom_navigation_bar_ui.dart
+++ lib/pages/bottom_navigation_bar_ui.dart
@@ -57,39 +57,98 @@ ProfileController profileController = Get.put(ProfileController());
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundBlack,
-      bottomNavigationBar: CurvedNavigationBar(
-        index: 0,
-        height: Platform.isIOS ? 75 : 60,
-        buttonBackgroundColor: AppColors.yellow,
-        animationCurve: Curves.fastEaseInToSlowEaseOut,
-        backgroundColor: AppColors.backgroundBlack,
-        color: AppColors.yellow,
-        animationDuration: const Duration(milliseconds: 500),
-        onTap: (index) {
-          setState(() {
-            widget.currentTabIndex = index;
-            currentPage = pages[widget.currentTabIndex!];
-          });
-        },
-        items: [
-          CurvedNavigationBarItem(
-            child:    Image.asset(ImagesPaths.home,scale: 5,),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFFFDF7B9),
+              Color(0xFFFEE290),
+              Color(0xFFD6BB7F),
+              Color(0xFFD4B061),
+              Color(0xFFFFD36B),
+              Color(0xFFFFF1B1)
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
-           CurvedNavigationBarItem(
-            child:  Image.asset(ImagesPaths.earningHome,scale: 6,),
-          ),
-           CurvedNavigationBarItem(
-            child: Image.asset(ImagesPaths.mysession,scale: 5,color: AppColors.black,),
-          ),
-          CurvedNavigationBarItem(
-            child:
-              Image.asset(ImagesPaths.men,scale: 5,),
+
+
+        ),
+
+        child:
+        BottomNavigationBar(
+          elevation: 100,
+          currentIndex: 0,
+          selectedItemColor: AppColors.black,
+          unselectedItemColor: Colors.black,
+          backgroundColor: Colors.transparent,
+          type: BottomNavigationBarType.fixed,
+          selectedFontSize: 12,
+          unselectedFontSize: 12,
+          selectedLabelStyle: TextStyle(color: AppColors.black),
+          unselectedLabelStyle: TextStyle(color: AppColors.black),
+          onTap: (index) {
+            setState(() {
+              widget.currentTabIndex = index;
+              currentPage = pages[widget.currentTabIndex!];
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              backgroundColor: Colors.transparent,
+              icon: Image.asset(ImagesPaths.home, scale: 5),
+              label: 'Home',
             ),
-          //  CurvedNavigationBarItem(
-          //   child: Image.asset(ImagesPaths.menu,scale: 5,),
-          // ),
-        ],
+            BottomNavigationBarItem(
+              icon: Image.asset(ImagesPaths.earningHome, scale: 7),
+              label: 'Earnings',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(ImagesPaths.mysession, color: Colors.black,scale: 5),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(ImagesPaths.men, scale: 5),
+              label: 'Account',
+            ),
+          ],
+        ),
       ),
+    // return Scaffold(
+    //   backgroundColor: AppColors.backgroundBlack,
+    //   bottomNavigationBar: CurvedNavigationBar(
+    //     index: 0,
+    //     height: Platform.isIOS ? 75 : 60,
+    //     buttonBackgroundColor: AppColors.yellow,
+    //     animationCurve: Curves.fastEaseInToSlowEaseOut,
+    //     backgroundColor: AppColors.backgroundBlack,
+    //     color: AppColors.yellow,
+    //     animationDuration: const Duration(milliseconds: 500),
+    //     onTap: (index) {
+    //       setState(() {
+    //         widget.currentTabIndex = index;
+    //         currentPage = pages[widget.currentTabIndex!];
+    //       });
+    //     },
+    //     items: [
+    //       CurvedNavigationBarItem(
+    //         child:    Image.asset(ImagesPaths.home,scale: 5,),
+    //       ),
+    //        CurvedNavigationBarItem(
+    //         child:  Image.asset(ImagesPaths.earningHome,scale: 6,),
+    //       ),
+    //        CurvedNavigationBarItem(
+    //         child: Image.asset(ImagesPaths.mysession,scale: 5,color: AppColors.black,),
+    //       ),
+    //       CurvedNavigationBarItem(
+    //         child:
+    //           Image.asset(ImagesPaths.men,scale: 5,),
+    //         ),
+    //       //  CurvedNavigationBarItem(
+    //       //   child: Image.asset(ImagesPaths.menu,scale: 5,),
+    //       // ),
+    //     ],
+    //   ),
       body: IndexedStack(
         index: widget.currentTabIndex,
         children: pages,
