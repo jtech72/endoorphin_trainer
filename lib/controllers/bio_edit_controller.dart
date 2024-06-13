8f3def2cecf1b058e0981109709d641a235defc9
ritanshu <ritanshu97@gmail.com>
2024-06-13 18:32:15 +0530

    bio edit and message ui

--- lib/controllers/bio_edit_controller.dart
+++ lib/controllers/bio_edit_controller.dart
@@ -1,5 +1,51 @@
+import 'dart:developer';
+
 import 'package:endoorphin_trainer/utils/exports.dart';
 
+import '../services/models/request_models/category_document_model.dart';
+import '../services/network_services/api_call.dart';
+
 class BioEditController extends GetxController{
+  TextEditingController niceNameController = TextEditingController();
+  TextEditingController professionalTitleController = TextEditingController();
+  TextEditingController experienceController = TextEditingController();
+  TextEditingController funFactController = TextEditingController();
+  TextEditingController motivationController = TextEditingController();
+  TextEditingController bioController = TextEditingController();
+
+  Future<void> onUpdateButton() async {
+    showLoader();
+    Map<String, String> fields = {
+      "id":storage.read("userId").toString(),
+      "nickName": niceNameController.text.trim(),
+      "professionalTitle": professionalTitleController.text.trim(),
+      "yearExperience": experienceController.text.trim(),
+      "funFact": funFactController.text.trim(),
+      "quote": motivationController.text.trim(),
+      "bio": bioController.text.trim(),
+    };
+
+    log("fields = $fields ");
+    try {
+      DocumentModel result = await CallAPI.uploadProfileDetails(
+        fields: fields,
+        files: {},
+      );
+      if (result.status == 200) {
+        dismissLoader();
+        Get.back();
+        showSnackBar(result.message.toString());
+        log("Successfully uploaded");
+      } else {
+        dismissLoader();
+        showSnackBar(result.message.toString());
+        log("Failed to upload");
+      }
+    } catch (e, st) {
+      log(e.toString());
+      log(st.toString());
+    }
+
+  }
 
 }
--- lib/services/models/response_models/profile_detail_model.dart
+++ lib/services/models/response_models/profile_detail_model.dart
@@ -24,46 +24,114 @@ class ProfileDetailsModel {
 }
 
 class Result {
+  int? id;
   String? userName;
-  String? email;
   String? lastName;
   String? password;
+  String? email;
   String? phoneNumber;
-  String? bio;
+  String? token;
+  dynamic? sessionCode;
   String? gender;
+  String? deviceId;
   String? profileImg;
+  dynamic? creditAmount;
+  dynamic? activeStatus;
+  dynamic? otp;
+  bool? status;
+  String? nickName;
+  String? professionalTitle;
+  String? yearExperience;
+  String? areaExpertise;
+  String? funFact;
+  String? quote;
+  String? bio;
+  String? createdAt;
+  String? updatedAt;
+  int? roleId;
 
   Result(
-      {this.userName,
-        this.email,
+      {this.id,
+        this.userName,
         this.lastName,
         this.password,
+        this.email,
         this.phoneNumber,
-        this.bio,
+        this.token,
+        this.sessionCode,
         this.gender,
-        this.profileImg});
+        this.deviceId,
+        this.profileImg,
+        this.creditAmount,
+        this.activeStatus,
+        this.otp,
+        this.status,
+        this.nickName,
+        this.professionalTitle,
+        this.yearExperience,
+        this.areaExpertise,
+        this.funFact,
+        this.quote,
+        this.bio,
+        this.createdAt,
+        this.updatedAt,
+        this.roleId});
 
   Result.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
     userName = json['userName'];
-    email = json['email'];
     lastName = json['lastName'];
     password = json['password'];
+    email = json['email'];
     phoneNumber = json['phoneNumber'];
-    bio = json['bio'];
+    token = json['token'];
+    sessionCode = json['sessionCode'];
     gender = json['gender'];
+    deviceId = json['deviceId'];
     profileImg = json['profileImg'];
+    creditAmount = json['creditAmount'];
+    activeStatus = json['activeStatus'];
+    otp = json['otp'];
+    status = json['status'];
+    nickName = json['nickName'];
+    professionalTitle = json['professionalTitle'];
+    yearExperience = json['yearExperience'];
+    areaExpertise = json['areaExpertise'];
+    funFact = json['funFact'];
+    quote = json['quote'];
+    bio = json['bio'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+    roleId = json['roleId'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
     data['userName'] = userName;
-    data['email'] = email;
     data['lastName'] = lastName;
     data['password'] = password;
+    data['email'] = email;
     data['phoneNumber'] = phoneNumber;
-    data['bio'] = bio;
+    data['token'] = token;
+    data['sessionCode'] = sessionCode;
     data['gender'] = gender;
+    data['deviceId'] = deviceId;
     data['profileImg'] = profileImg;
+    data['creditAmount'] = creditAmount;
+    data['activeStatus'] = activeStatus;
+    data['otp'] = otp;
+    data['status'] = status;
+    data['nickName'] = nickName;
+    data['professionalTitle'] = professionalTitle;
+    data['yearExperience'] = yearExperience;
+    data['areaExpertise'] = areaExpertise;
+    data['funFact'] = funFact;
+    data['quote'] = quote;
+    data['bio'] = bio;
+    data['createdAt'] = createdAt;
+    data['updatedAt'] = updatedAt;
+    data['roleId'] = roleId;
     return data;
   }
 }
