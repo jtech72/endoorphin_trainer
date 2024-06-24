e3811fecccd421e538fd76dd39296271763fca44
suraj bhandari <surajbhandari@192.168.1.11>
2024-06-24 09:50:27 +0530

    bank detail apis

--- lib/controllers/trainer_passport_controller.dart
+++ lib/controllers/trainer_passport_controller.dart
@@ -2,29 +2,29 @@ import 'dart:developer';
 import 'package:endoorphin_trainer/utils/exports.dart';
 import 'package:get/get.dart';
 
-class TrainerPassportController extends GetxController{
-  Map<String, dynamic>? certificationDetails ;
+class TrainerPassportController extends GetxController {
+  Map<String, dynamic>? certificationDetails;
   TextEditingController certificateName = TextEditingController();
   TextEditingController certificateNumber = TextEditingController();
 
- onFileUpload(){
-   if(certificateNumber.text.isEmpty||certificateName.text.isEmpty){
-     showSnackBar("Please enter a certificate number and name");
-   }else{
-     Get.toNamed(AppRoutes.uploadimage,arguments: {
-       "userId": certificationDetails!["userId"],
-       "categoryName": certificationDetails!["categoryName"],
-       "categoryId": certificationDetails!["categoryId"],
-       "certificateNumber": certificateNumber.text,
-       "certificateName": certificateName.text,
-     });
+  onFileUpload() {
+    if (certificateNumber.text.isEmpty || certificateName.text.isEmpty) {
+      showSnackBar("Please enter a certificate number and name");
+    } else {
+      Get.toNamed(AppRoutes.uploadimage, arguments: {
+        "userId": certificationDetails!["userId"],
+        "categoryName": certificationDetails!["categoryName"],
+        "categoryId": certificationDetails!["categoryId"],
+        "certificateNumber": certificateNumber.text,
+        "certificateName": certificateName.text,
+      });
+    }
+  }
 
-   }
- }
   @override
   void onInit() {
-    certificationDetails = Get.arguments ?? "";
-    log(certificationDetails.toString());
     super.onInit();
+    certificationDetails = Get.arguments;
+    log(certificationDetails.toString());
   }
 }
