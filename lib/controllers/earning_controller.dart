554ae4675d9edff4ddc7973b030a06df0bc09688
suraj bhandari <surajbhandari@192.168.1.14>
2024-07-02 18:23:20 +0530

    resolve bugs

--- lib/controllers/earning_controller.dart
+++ lib/controllers/earning_controller.dart
@@ -4,7 +4,8 @@ class EarningController extends GetxController{
   final List<String> months = [
     'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
   ];
-  final List<String> days = ['1-7','8-14','15-21','21-28','29-31'];
+  final List<String> days = ['1-7','8-14','15-21','21-28','29-31'  ];
+  /////
   var isWeekly = true.obs;
   Rx<int> tappedIndex = (-1).obs;
   void toggleView() {
