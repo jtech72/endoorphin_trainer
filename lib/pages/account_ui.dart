d6797c5cc26e3118610db7e54e3f18ba75abdee7
nisha <n.negi@rowthtech.com>
2024-09-02 11:44:19 +0530

    init

--- lib/pages/account_ui.dart
+++ lib/pages/account_ui.dart
@@ -3,8 +3,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:endoorphin_trainer/utils/exports.dart';
 import 'package:flutter/material.dart';
 import '../controllers/account_controller.dart';
-import '../services/network_services/api_call.dart';
-
 class AccountUI extends StatelessWidget {
   const AccountUI({super.key});
   @override
